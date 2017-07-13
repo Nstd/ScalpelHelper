bbc2d5cb7db16a1bb92660ce58b494c66783db52
maoting <maoting@haizitong.com>
2017-07-13 10:14:23 +0800

    test databinding

--- app/src/main/java/com/nstd/testscalpel/MainActivity.java
+++ app/src/main/java/com/nstd/testscalpel/MainActivity.java
@@ -1,19 +1,27 @@
 package com.nstd.testscalpel;
 
+import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.github.nstd.scalpelhelper.ScalpelHelper;
+import com.nstd.testscalpel.databinding.ActivityMainBinding;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
+
+    ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        Toast.makeText(this, "mBinding is null = " + (mBinding == null), Toast.LENGTH_SHORT).show();
     }
 
     @Override
